3b9043807cda04e767480708d86f92e580ff1189
Axel Anderson <axel@ftml.net>
2017-11-22 17:45:34 -0500

    fix notes line height issue

--- views/notes.js
+++ views/notes.js
@@ -4,7 +4,7 @@ const Link = require('../components/Link')
 
 function Item (note) {
   return html`
-    <li class="mb2-ns mb1">
+    <li>
       ${Link({ href: `/notes/${note.slug}`, text: note.title })}
     </li>
   `
@@ -17,7 +17,7 @@ module.exports = function notesView (state, emit) {
   }
 
   const children = html`
-    <article class="notes">
+    <article class="notes lh-copy">
       <ul class="list mt0 mb3 pl5">
         ${state.notes.map(note => Item(note))}
       </ul>
