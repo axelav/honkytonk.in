117981852df1c38f30e8f59d6c13aa7ded5fa5cb
Axel Anderson <axel@ftml.net>
2021-01-25 11:55:53 -0500

    feat: add git SHA w/ github link to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Link from 'next/link'
 import Header from './Header'
 
 const Layout = ({ children }) => (
-  <div className="Layout sans-serif near-black f5 mw7-ns mt4 mb7 center ph2 ph4-m ph0-l">
-    <Header />
-    {children}
+  <div className="Layout sans-serif near-black f5">
+    <div className="mw7-ns mt4 mb7 center ph2 ph4-m ph0-l">
+      <Header />
+      {children}
+    </div>
+
+    {process.env.NEXT_PUBLIC_SHA && (
+      <div className="tr pa4">
+        <Link
+          href={`https://github.com/axelav/honkytonk.in/commit/${process.env.NEXT_PUBLIC_SHA}`}
+        >
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="link near-white hover-moon-gray"
+          >
+            {process.env.NEXT_PUBLIC_SHA.substr(0, 8)}
+          </a>
+        </Link>
+      </div>
+    )}
   </div>
 )
 
