dd42b960295c842c414959ff3b0e133384af5d98
Axel Anderson <axel@ftml.net>
2021-10-26 17:59:31 -0400

    feat: use faunadb

--- pages/api/session.js
+++ pages/api/session.js
@@ -1,14 +1,12 @@
-const { Pool } = require('pg')
+const faunadb = require('faunadb')
+const q = faunadb.query
 
-const credentials = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-}
-
-const pool = new Pool(credentials)
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_SECRET_KEY,
+  domain: 'db.us.fauna.com',
+  port: 443,
+  scheme: 'https',
+})
 
 module.exports = async (req, res) => {
   const {
@@ -26,56 +24,44 @@ module.exports = async (req, res) => {
     pageviewCount,
   } = JSON.parse(req.body)
 
-  const client = await pool.connect()
-
   try {
-    await client.query('BEGIN')
-
-    const insertSessionText =
-      'INSERT INTO sessions(started_at, ended_at, ref, device, user_agent, language, time_zone, latency, page_load, length, pageview_count) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *'
-    const insertSessionValues = [
-      startedAt,
-      endedAt,
-      ref,
-      device,
-      userAgent,
-      language,
-      timeZone,
-      latency,
-      pageLoad,
-      length,
-      pageviewCount,
-    ]
-
-    const sessionResult = await client.query(
-      insertSessionText,
-      insertSessionValues
+    const { ref: sessionRef } = await client.query(
+      q.Create(q.Collection('_sessions'), {
+        data: {
+          startedAt,
+          endedAt,
+          ref,
+          device,
+          userAgent,
+          language,
+          timeZone,
+          latency,
+          pageLoad,
+          length,
+          pageviewCount,
+        },
+      })
     )
 
     await Promise.all(
       events.map(async ({ timestamp, event, type, length }) => {
-        const insertEventText =
-          'INSERT INTO events(session_id, timestamp, event, type, length) VALUES ($1, $2, $3, $4, $5) RETURNING *'
-        const insertEventValues = [
-          sessionResult.rows[0].id,
-          timestamp,
-          event,
-          type,
-          length,
-        ]
-
-        await client.query(insertEventText, insertEventValues)
+        await client.query(
+          q.Create(q.Collection('_events'), {
+            data: {
+              sessionRef,
+              timestamp,
+              event,
+              type,
+              length,
+            },
+          })
+        )
       })
     )
 
-    await client.query('COMMIT')
-
     res.status(201).end()
   } catch (err) {
-    await client.query('ROLLBACK')
     res.status(500).end()
     console.error('error running query', err)
-  } finally {
-    client.release()
   }
 }
