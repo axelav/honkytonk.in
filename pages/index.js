53c1af45b58428d5bf3ce53d72b2482eec151c01
axelav <axel@ftml.net>
2022-09-25 10:10:55 -0400

    Remove video from index
    wtf is going on?

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Title } from '../components/provider'
 import Layout from '../components/Layout'
 import IndexLink from '../components/IndexLink'
-import Video from '../components/Video'
-
-const honkyTonkVideoIds = [
-  // Hank's classic
-  'kN17OTQIGqg',
-  '6c3K-LIo_gQ',
-  'KHEQi25GSNo',
-  'BrkLxJqb2xM',
-  '918TER9fDRQ',
-  'Y0r651OMVUw',
-  'FxKuAMmYkOo',
-  'sODtr7C2u4M',
-  'Orr0ZDUii9o',
-  // others
-  'o1R4ls73dxU',
-  'prKDa0-uvBA',
-]
-
-const getRandomVideoId = () =>
-  honkyTonkVideoIds[Math.floor(Math.random() * honkyTonkVideoIds.length)]
 
 const Index = () => {
-  const [videoId, setVideoId] = useState(getRandomVideoId())
-
   return (
     <Layout>
       <Title>Lately</Title>
@@ -69,17 +47,6 @@ const Index = () => {
         textLeft="Trans America Trail"
         textRight="2018-10-23"
       />
-      <hr />
-      <Video
-        className="center"
-        src={`https://www.youtube.com/embed/${videoId}`}
-      />
-      <span
-        className="db link blue hover-dark-blue tc pointer"
-        onClick={() => setVideoId(getRandomVideoId())}
-      >
-        Find another dance partner
-      </span>
     </Layout>
   )
 }
