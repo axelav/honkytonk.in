189b3799d5ba53ebd1acf2d188665a4a79444939
Axel Anderson <axel@ftml.net>
2023-09-18 18:14:49 -0400

    Collect some basic user attrs w/ KV

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,13 +1,6 @@
 import { MiddlewareHandlerContext } from '$fresh/server.ts'
 
-interface State {
-  data: { [key: string]: any }
-}
-
-export async function handler(
-  _req: Request,
-  ctx: MiddlewareHandlerContext<State>
-) {
+export async function handler(_req: Request, ctx: MiddlewareHandlerContext) {
   const res = await ctx.next()
 
   res.headers.set('server', "fresh honky tonkin'")
@@ -15,9 +8,16 @@ export async function handler(
   if (ctx.destination === 'route') {
     const kv = await Deno.openKv()
 
-    const entry = await kv.get<number>(['analytics', 'pageviews'])
+    const pageviews = await kv.get<number>(['analytics', 'pageviews'])
 
-    await kv.set(['analytics', 'pageviews'], (entry.value ?? 0) + 1)
+    await kv.set(['analytics', 'pageviews'], (pageviews.value ?? 0) + 1)
+
+    await kv.set(['analytics', 'userAttributes'], {
+      ref: document.referrer,
+      userAgent: navigator.userAgent,
+      language: navigator.language,
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    })
   }
 
   return res
