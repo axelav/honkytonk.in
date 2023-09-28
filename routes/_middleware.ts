0c09e450f302fc7846f10e83e725d24686095ec6
Axel Anderson <axel@ftml.net>
2023-09-28 10:25:45 -0400

    Log pathname & referrer in middleware

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,6 +1,6 @@
 import { MiddlewareHandlerContext } from '$fresh/server.ts'
 
-export async function handler(_req: Request, ctx: MiddlewareHandlerContext) {
+export async function handler(req: Request, ctx: MiddlewareHandlerContext) {
   const res = await ctx.next()
 
   res.headers.set('server', "fresh honky tonkin'")
@@ -9,6 +9,8 @@ export async function handler(_req: Request, ctx: MiddlewareHandlerContext) {
     const kv = await Deno.openKv()
 
     const pageviews = await kv.get<number>(['analytics', 'pageviews'])
+    const url = new URL(req.url)
+    console.log('Pageview:', url.pathname, req.referrer)
 
     await kv.set(['analytics', 'pageviews'], (pageviews.value ?? 0) + 1)
   }
