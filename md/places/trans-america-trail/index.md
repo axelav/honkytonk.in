ae27a91081a0f77d2f5bafe5f824bf49a620f6fa
Axel Anderson <axel@ftml.net>
2023-10-03 07:18:49 -0400

    Clean up

--- md/places/trans-america-trail/index.md
+++ md/places/trans-america-trail/index.md
@@ -3,5 +3,3 @@ title: Trans America Trail
 published_at: 2018-08-18T15:00:00.000Z
 snippet: August – September 2018
 ---
-
-FIXME: don't want this to render in the list of pages!
--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -6,11 +6,12 @@ export async function handler(req: Request, ctx: MiddlewareHandlerContext) {
   res.headers.set('server', "fresh honky tonkin'")
 
   if (ctx.destination === 'route') {
+    const url = new URL(req.url)
+    console.log('Pageview:', url.pathname, req.referrer)
+
     const kv = await Deno.openKv()
 
     const pageviews = await kv.get<number>(['analytics', 'pageviews'])
-    const url = new URL(req.url)
-    console.log('Pageview:', url.pathname, req.referrer)
 
     await kv.set(['analytics', 'pageviews'], (pageviews.value ?? 0) + 1)
   }
